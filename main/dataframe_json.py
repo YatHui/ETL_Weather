cba92a39962c1fc27607990c22063a24ef6b3904
Araz Nabi <araznabi@gmail.com>
2023-10-10 14:11:41 +0200

    changes to dataframe_json.py

--- main/dataframe_json.py
+++ main/dataframe_json.py
@@ -1,18 +1,16 @@
 import pandas as pd
 import json
 import matplotlib.pyplot as plt
-import datetime 
+from datetime import datetime
 
-json_file_path = './main/raw_data.json'
+def find_temperature():
+    json_file_path = './main/raw_data.json'
+    # Read the JSON data from the file
+    with open(json_file_path, 'r') as json_file:
+        json_data = json.load(json_file)
 
-# Read the JSON data from the file
-with open(json_file_path, 'r') as json_file:
-    json_data = json.load(json_file)
-
-# Create an empty list to store the data
-data = []
-
-def find_temperature(json_data):
+    # Create an empty list to store the data
+    data = []
     # Itirate through timeseries-data
     for series in json_data[0]['timeSeries']:
         valid_time = series['validTime']
@@ -22,25 +20,37 @@ def find_temperature(json_data):
                 temperature = param['values'][0]
                 date, time = valid_time.split('T')
                 data.append({'validDate': date, 'validTime': time, 'temperature': temperature})
-
-
-
-def clean_time(data):
-    #Use todays date
-    date_object = datetime.datetime.now().date()
-    specific_date = date_object.strftime("%Y-%m-%d")
-    # Create a DataFrame from the collected data
+    
     df = pd.DataFrame(data)
-    # Remove the "Z" from the time column
-    df['validTime'] = df['validTime'].str.replace('Z', '')
+    # write json file
+    df.to_json('./etl_data/temperature_data.json', orient='records', indent=4)
+    
 
-    # df['validTime'] = df['validTime'].str.replace(':00', '')
-    df['validTime'] = df['validTime'].str.slice(0, 5)
-    filtered_df = df[df['validDate'] == specific_date]
-    return filtered_df
+def clean_time():
+    json_file_path = './etl_data/temperature_data.json'
+    # Read the JSON data from the file
+    with open(json_file_path, 'r') as json_file:
+        json_data = json.load(json_file)
+        
+        #Use todays date
+        date_object = datetime.now().date()
+        specific_date = date_object.strftime("%Y-%m-%d")
+        
+        # Create a DataFrame from the collected data
+        df = pd.DataFrame(json_data)
+        
+        # Remove the "Z" from the time column
+        df['validTime'] = df['validTime'].str.replace('Z', '')
+
+        # df['validTime'] = df['validTime'].str.replace(':00', '')
+        df['validTime'] = df['validTime'].str.slice(0, 5)
+        filtered_df = df[df['validDate'] == specific_date]
+        
+        # write json file
+        filtered_df.to_json('./etl_data/cleaned_data.json', orient='records', indent=4)
 
 
 if __name__=="__main__":
-    find_temperature(json_data)
-    test = clean_time(data)
+    find_temperature()
+    test = clean_time()
     print(test)
